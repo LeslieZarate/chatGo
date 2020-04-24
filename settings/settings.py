7fbb945c3acd07d8d0fd3488ad7966e982959661
LeslieZarate <fiorelly30zarate@gmail.com>
2020-04-24 13:20:23 -0500

    Procfile

--- settings/settings.py
+++ settings/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        "BACKEND": "asgi_redis.RedisChannelLayer",
         'CONFIG': {
             "hosts": ["redis://:dq0AZxdTloU41OTKmvcp44blxm4O5gSz@redis-11479.c17.us-east-1-4.ec2.cloud.redislabs.com:11479/0"],
              "symmetric_encryption_keys": [SECRET_KEY],
